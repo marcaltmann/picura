663ff8a8b5d1c8ee67be959b500cf37d21746a7b
Marc Altmann <marcaltmann@posteo.de>
2026-05-25 21:02:22 +0200

    Add public website link to sidebar and fix debug toolbar INTERNAL_IPS

--- curio/settings.py
+++ curio/settings.py
@@ -51,6 +51,7 @@ if DJANGO_ENV == 'development':
     INSTALLED_APPS += [
         'debug_toolbar',
     ]
+    INTERNAL_IPS = ['127.0.0.1']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
