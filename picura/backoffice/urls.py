6f09062274557b4cf288b0d99c629137f789db4b
Marc Altmann <marcaltmann@posteo.de>
2026-05-31 00:13:09 +0200

    Drop /content/ prefix from backoffice photo URLs

--- picura/backoffice/urls.py
+++ picura/backoffice/urls.py
@@ -6,19 +6,13 @@ urlpatterns = [
     path('', views.dashboard, name='backoffice_dashboard'),
     path('batches/', views.batch_list, name='backoffice_batch_list'),
     path('batches/<int:pk>/', views.batch_detail, name='backoffice_batch_detail'),
-    path('content/photos/', views.photo_list, name='backoffice_photo_list'),
+    path('photos/', views.photo_list, name='backoffice_photo_list'),
+    path('photos/<int:pk>/', views.photo_detail, name='backoffice_photo_detail'),
+    path('photos/upload/', views.photo_upload, name='backoffice_photo_upload'),
     path(
-        'content/photos/<int:pk>/', views.photo_detail, name='backoffice_photo_detail'
-    ),
-    path('content/photos/upload/', views.photo_upload, name='backoffice_photo_upload'),
-    path(
-        'content/photos/bulk-delete/',
+        'photos/bulk-delete/',
         views.photo_bulk_delete,
         name='backoffice_photo_bulk_delete',
     ),
-    path(
-        'content/photos/<int:pk>/delete/',
-        views.photo_delete,
-        name='backoffice_photo_delete',
-    ),
+    path('photos/<int:pk>/delete/', views.photo_delete, name='backoffice_photo_delete'),
 ]
